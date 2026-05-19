04ec31a8586461a1ef2a938a1a1abe6959acf821
Felipe Amoroso Lambaz <felipe.lambaz@sptech.school>
2026-05-18 23:00:06 -0300

    Update resultadosController.js

--- src/controllers/resultadosController.js
+++ src/controllers/resultadosController.js
@@ -1,45 +1,36 @@
 var resultadoModel = require("../models/resultadosModel");
 
 function salvar(req, res) {
-    var fkUsuario = req.body.fkUsuario;
-    var fkTentativa = req.body.fkTentativa; 
-    var acertos = req.body.acertos;
-    var erros = req.body.erros;
+    var fkUsuario = Number(req.body.fkUsuario);
+    var fkTentativa = Number(req.body.fkTentativa);
+    var acertos = Number(req.body.acertos);
+    var erros = Number(req.body.erros);
 
- 
-    console.log("Dados recebidos no controller:", {
+    console.log("Dados recebidos:", {
         fkUsuario,
         fkTentativa,
         acertos,
         erros
     });
 
-    if (fkUsuario == undefined || fkTentativa == undefined) {
-        res.status(400).send("Dados inválidos (fkUsuario ou fkTentativa undefined)");
+    if (fkUsuario == undefined || fkTentativa == undefined || acertos == undefined || erros == undefined) {
+        res.status(400).send("Dados inválidos");
         return;
     }
 
     resultadoModel.salvar(fkUsuario, fkTentativa, acertos, erros)
-        .then(function (resultado) {
-            res.status(200).json(resultado);
-        })
-        .catch(function (erro) {
-            console.log("Erro ao salvar resultado:", erro);
+        .then(resultado => res.json(resultado))
+        .catch(erro => {
+            console.log("Erro:", erro);
             res.status(500).json(erro);
         });
 }
 
 function listar(req, res) {
     var idUsuario = req.params.idUsuario;
-
     resultadoModel.listar(idUsuario)
-        .then(function (resultado) {
-            res.json(resultado);
-        })
-        .catch(function (erro) {
-            console.log("Erro ao listar:", erro);
-            res.status(500).json(erro);
-        });
+        .then(resultado => res.json(resultado))
+        .catch(erro => res.status(500).json(erro));
 }
 
 module.exports = {
